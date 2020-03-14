2bc347c81dab565474c2d0278a3dd1a2f03eaad4
Mr <919553308@qq.com>
2020-03-14 18:13:05 +0800

    1

--- src/page/business/BusinessNote.vue
+++ src/page/business/BusinessNote.vue
@@ -139,25 +139,6 @@ export default {
 
 <style lang="scss">
 @import '../../assets/scss/mixin.scss';
-.v-enter,
-.v-leave-to {
-    opacity: 0;
-    transform: translateY(80px);
-}
-
-.v-enter-active,
-.v-leave-active {
-    transition: all 0.6s ease;
-}
-
-/* 下面的 .v-move 和 .v-leave-active 配合使用，能够实现列表后续的元素，渐渐地漂上来的效果 */
-.v-move {
-    transition: all 0.6s ease;
-}
-
-.v-leave-active {
-    position: absolute;
-}
 .business-note{
   width: 1260px;
   background-color: #FCFCFC;
