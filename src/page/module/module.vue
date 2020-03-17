ffce13fd3caf56ce72a2d11bd7d9566784a6f8de
linxiaoli <lin1203@foxmail.com>
2020-03-17 12:14:59 +0800

    merge enterprise

--- src/page/module/module.vue
+++ src/page/module/module.vue
@@ -1,29 +1,21 @@
 <template>
   <div>
-<<<<<<< HEAD
     <module-content :styleContent="{padding:'20px'}">
     <module-button></module-button>
     <module-table></module-table>
     </module-content>
-=======
-    <Content :styleContent="{padding:'20px'}">
-
-    <module-button slot="head"></module-button>
-    <module-table slot="body"></module-table>
-    </Content>
->>>>>>> 2a0a00f23768caf46c08ed9a47451804b7e0b37f
   </div>
 </template>
 
 <script>
 import ModuleButton from './childCpns/ModuleButton'
-// import ModuleContent from './childCpns/ModuleContent'
+import ModuleContent from './childCpns/ModuleContent'
 import ModuleTable from './childCpns/ModuleTable'
 export default {
   name: 'module',
   components: {
     ModuleButton,
-    // ModuleContent,
+    ModuleContent,
     ModuleTable
   }
 }
