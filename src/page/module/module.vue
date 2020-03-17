a2828e84b7d47f507823ebfcfd06f9a7e6308a31
linxiaoli <lin1203@foxmail.com>
2020-03-17 12:08:33 +0800

    before merge

--- src/page/module/module.vue
+++ src/page/module/module.vue
@@ -1,17 +1,9 @@
 <template>
   <div>
-<<<<<<< HEAD
-    <Content :styleContent="{padding:'20px'}">
-
-    <module-button slot="head"></module-button>
-    <module-table slot="body"></module-table>
-    </Content>
-=======
     <module-content :styleContent="{padding:'20px'}">
     <module-button></module-button>
     <module-table></module-table>
     </module-content>
->>>>>>> 9b638adbab5c0f891273b1dbcc42699a1f42450b
   </div>
 </template>
 
